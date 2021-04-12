bd343bdf8e72ea05679bc18f2d28b9509b536934
Keven Silva <keven.sp@hotmail.com>
2021-04-12 20:03:04 -0300

    Criando repository e service para classe Categoria

---
+++ src/main/java/com/keven/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.keven.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.keven.cursomc.domain.Categoria;
+import com.keven.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
