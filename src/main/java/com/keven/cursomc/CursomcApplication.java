ed406f67ff51940cc94db4874b943c5c892ba38d
Keven Silva <keven.sp@hotmail.com>
2021-04-16 01:44:56 -0300

    Criação tab e Assos. de cliente, endereço e telefone + Enum

--- src/main/java/com/keven/cursomc/CursomcApplication.java
+++ src/main/java/com/keven/cursomc/CursomcApplication.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties.ClientType;
 
 import com.keven.cursomc.domain.Categoria;
 import com.keven.cursomc.domain.Cidade;
+import com.keven.cursomc.domain.Cliente;
+import com.keven.cursomc.domain.Endereco;
 import com.keven.cursomc.domain.Estado;
 import com.keven.cursomc.domain.Produto;
+import com.keven.cursomc.domain.enums.TipoCliente;
 import com.keven.cursomc.repositories.CategoriaRepository;
 import com.keven.cursomc.repositories.CidadeRepository;
+import com.keven.cursomc.repositories.ClienteRepository;
+import com.keven.cursomc.repositories.EnderecoRepository;
 import com.keven.cursomc.repositories.EstadoRepository;
 import com.keven.cursomc.repositories.ProdutoRepository;
 
@@ -27,27 +33,34 @@ public class CursomcApplication implements CommandLineRunner{
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired 
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
-		
-		Categoria cat1 = new Categoria(1, "Informatica");
-		Categoria cat2 = new Categoria(2, "Escritorio");
+	public void run(String... args) throws Exception {			
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().add(p2);
+		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat1));		
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -61,7 +74,18 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Cliente cli1 = new Cliente(null, "Maria", "maria@gmail.com", "3637650965", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("54153435", "5213512"));
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 303", "Jardim", "6265463", cli1, c1);
+		Endereco e2 = new Endereco(null, "Rua Delta", "100", "Apto 403", "Jardim", "6265463", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e1));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		
 	}
 }
--- src/main/java/com/keven/cursomc/domain/Cidade.java
+++ src/main/java/com/keven/cursomc/domain/Cidade.java
@@ -1,6 +1,8 @@
 package com.keven.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Cidade implements Serializable{
@@ -18,6 +21,9 @@ public class Cidade implements Serializable{
 	private Integer id;
 	
 	private String nome;
+	
+	@OneToMany
+	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ManyToOne
 	@JoinColumn(name="estado_id")
