e6294a2015b551a613bdb4e0c50af549dd307f46
Keven Silva <keven.sp@hotmail.com>
2021-04-19 18:22:22 -0300

    Listando tds categorias com dto

--- src/main/java/com/keven/cursomc/services/CategoriaService.java
+++ src/main/java/com/keven/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.keven.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,7 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -39,5 +41,9 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir categoria que tenha produto");
 		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
