a25ccef703b399803d51a4ab5502d8e50cb1df94
Keven Silva <keven.sp@hotmail.com>
2021-04-19 01:37:43 -0300

    inserindo method DELETE com tratamento de exc DataIntegrity

--- src/main/java/com/keven/cursomc/services/CategoriaService.java
+++ src/main/java/com/keven/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.keven.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.keven.cursomc.domain.Categoria;
 import com.keven.cursomc.repositories.CategoriaRepository;
+import com.keven.cursomc.services.exceptions.DataIntegrityException;
 import com.keven.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +30,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir categoria que tenha produto");
+		}
 	}
 }
