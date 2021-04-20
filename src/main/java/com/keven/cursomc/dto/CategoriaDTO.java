883b2088fbe231017b168620f8bf86dd95b3326d
Keven Silva <keven.sp@hotmail.com>
2021-04-19 22:43:49 -0300

    Validação sintatica com Bean Validation @NotEmpty e @Length na categoriaDTO

--- src/main/java/com/keven/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/keven/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,18 @@ package com.keven.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
 import com.keven.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;	
+	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatorio")
+	@Length(min=5, max=80, message = "Tamanho entre 5 e 80 caractere")
 	private String nome;
 	
 	public CategoriaDTO() {}
