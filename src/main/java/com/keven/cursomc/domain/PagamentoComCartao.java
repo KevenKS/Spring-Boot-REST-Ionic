eb6d7cd8358ad3e8236a4fd8f25a99ed86a3ea86
Keven <keven.sp@hotmail.com>
2021-07-29 12:45:21 -0300

    Inserindo Pedido

--- src/main/java/com/keven/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/keven/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.keven.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.keven.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	private static final long serialVersionUID = 1L;
 	
--- src/main/java/com/keven/cursomc/services/PedidoService.java
+++ src/main/java/com/keven/cursomc/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.keven.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.keven.cursomc.domain.ItemPedido;
+import com.keven.cursomc.domain.PagamentoComBoleto;
 import com.keven.cursomc.domain.Pedido;
+import com.keven.cursomc.domain.enums.EstadoPagamento;
+import com.keven.cursomc.repositories.ItemPedidoRepository;
+import com.keven.cursomc.repositories.PagamentoRepository;
 import com.keven.cursomc.repositories.PedidoRepository;
 import com.keven.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -15,8 +21,41 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Obj não existe" + Pedido.class.getName()));
-	}	
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
+	}
 }
